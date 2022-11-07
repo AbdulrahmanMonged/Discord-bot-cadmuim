7a559ed7923a32892f38d0468db7f263aff53cfc
Bodyy Monged <abdoomonged231@gmail.com>
2022-11-07 14:38:46 +0200

    int

---
+++ cogs/interactions.py
@@ -0,0 +1,270 @@
+import discord
+from discord.ext import commands
+import requests
+from discord import Embed
+import random
+from urllib.parse import urlparse
+import psycopg
+import asyncio
+import os
+
+
+TENOR_API_KEY = os.getenv("TENOR_API_KEY")
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+DB_URI = os.getenv("DB_URI")
+db_uri = urlparse(DB_URI)
+host = db_uri.hostname
+database = db_uri.path[1:]
+user = db_uri.username
+password = db_uri.password
+port= db_uri.port
+
+
+class Interactions(commands.Cog): 
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        async with await psycopg.AsyncConnection.connect(host=host, dbname=database, user=user, password=password, port=port) as db:
+                    async with db.cursor() as cursor:
+                        await cursor.execute("""CREATE TABLE IF NOT EXISTS INTERACTIONS(
+            USER1_ID BIGINT NOT NULL,
+            USER2_ID BIGINT NOT NULL,
+            NUM_KISSES BIGINT,
+            NUM_HUGS BIGINT,
+            NUM_CUDDLES BIGINT,
+            NUM_SLAPS BIGINT,
+            NUM_PATS BIGINT,
+            NUM_LICKS BIGINT)""")
+                        
+                  
+    @commands.command(description="kisses a member")
+    async def kiss(self, ctx, *, member: discord.Member):
+        async with await psycopg.AsyncConnection.connect(host=host, dbname=database, user=user, password=password, port=port) as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("SELECT count(*) FROM INTERACTIONS where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                db_result = await cursor.fetchone()
+                if db_result[0] == 0:
+                    await cursor.execute(f"INSERT INTO INTERACTIONS VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (ctx.author.id, member.id, 0, 0, 0, 0, 0, 0,))
+                await db.commit()
+                await cursor.execute("SELECT NUM_KISSES FROM INTERACTIONS where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                results = await cursor.fetchone()
+                kisses = results[0] + 1
+                await cursor.execute("UPDATE INTERACTIONS set NUM_KISSES = NUM_KISSES + 1 where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                await db.commit()
+        apikey = TENOR_API_KEY
+        lmt = 20
+        ckey = "my_test_app"  
+        search_term = "anime kiss gif"
+        r = requests.get("https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (search_term, apikey, ckey,  lmt))
+        random_data = random.choice(r.json()["results"])
+        img = random_data["media_formats"]["gif"]["url"]
+        embed = Embed(title=f"{ctx.author.name} kisses {member.name} !", color=ctx.author.color, timestamp=ctx.message.created_at)
+        embed.set_image(url=img)
+        if ctx.author.avatar != None:
+            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
+        else:
+            embed.set_author(name=ctx.author.name)
+        embed.set_footer(text=f"That's {kisses} kisses !")
+        await ctx.send(embed=embed)
+        
+        
+    @commands.command(description="hugs a member")
+    async def hug(self, ctx, *, member: discord.Member):
+        async with await psycopg.AsyncConnection.connect(host=host, dbname=database, user=user, password=password, port=port) as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("SELECT count(*) FROM INTERACTIONS where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                db_result = await cursor.fetchone()
+                if db_result[0] == 0:
+                    await cursor.execute(f"INSERT INTO INTERACTIONS VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (ctx.author.id, member.id, 0, 0, 0, 0, 0, 0,))
+                await db.commit()
+                await cursor.execute("SELECT NUM_HUGS FROM INTERACTIONS where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                results = await cursor.fetchone()
+                hugs = results[0] + 1
+                await cursor.execute("UPDATE INTERACTIONS set NUM_HUGS = NUM_HUGS + 1 where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                await db.commit()
+        apikey = TENOR_API_KEY
+        lmt = 20
+        ckey = "my_test_app"  
+        search_term = "anime hug gif"
+        r = requests.get("https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (search_term, apikey, ckey,  lmt))
+        random_data = random.choice(r.json()["results"])
+        img = random_data["media_formats"]["gif"]["url"]
+        embed = Embed(title=f"{ctx.author.name} hugs {member.name} !", colour=ctx.author.color, timestamp=ctx.message.created_at)
+        embed.set_image(url=img)
+        if ctx.author.avatar != None:
+            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
+        else:
+            embed.set_author(name=ctx.author.name)
+        embed.set_footer(text=f"That's {hugs} hugs !")
+        await ctx.send(embed=embed)
+        
+    @commands.command(description="cuddles a member")
+    async def cuddle(self, ctx, *, member: discord.Member):
+        async with await psycopg.AsyncConnection.connect(host=host, dbname=database, user=user, password=password, port=port) as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("SELECT count(*) FROM INTERACTIONS where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                db_result = await cursor.fetchone()
+                if db_result[0] == 0:
+                    await cursor.execute(f"INSERT INTO INTERACTIONS VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (ctx.author.id, member.id, 0, 0, 0, 0, 0, 0,))
+                await db.commit()
+                await cursor.execute("SELECT NUM_CUDDLES FROM INTERACTIONS where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                results = await cursor.fetchone()
+                cuddles = results[0] + 1
+                await cursor.execute("UPDATE INTERACTIONS set NUM_CUDDLES = NUM_CUDDLES + 1 where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                await db.commit()
+        apikey = TENOR_API_KEY
+        lmt = 20
+        ckey = "my_test_app"  
+        search_term = "anime cuddle gif"
+        r = requests.get("https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (search_term, apikey, ckey,  lmt))
+        random_data = random.choice(r.json()["results"])
+        img = random_data["media_formats"]["gif"]["url"]
+        embed = Embed(title=f"{ctx.author.name} cuddles {member.name} !", colour=ctx.author.color, timestamp=ctx.message.created_at)
+        embed.set_image(url=img)
+        if ctx.author.avatar != None:
+            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
+        else:
+            embed.set_author(name=ctx.author.name)
+        embed.set_footer(text=f"That's {cuddles} cuddles !")
+        await ctx.send(embed=embed)
+        
+    @commands.command(description="slaps a member")
+    async def slap(self, ctx, *, member: discord.Member):
+        async with await psycopg.AsyncConnection.connect(host=host, dbname=database, user=user, password=password, port=port) as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("SELECT count(*) FROM INTERACTIONS where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                db_result = await cursor.fetchone()
+                if db_result[0] == 0:
+                    await cursor.execute(f"INSERT INTO INTERACTIONS VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (ctx.author.id, member.id, 0, 0, 0, 0, 0, 0,))
+                await db.commit()
+                await cursor.execute("SELECT NUM_SLAPS FROM INTERACTIONS where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                results = await cursor.fetchone()
+                slaps = results[0] + 1
+                await cursor.execute("UPDATE INTERACTIONS set NUM_SLAPS = NUM_SLAPS + 1 where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                await db.commit()
+        apikey = TENOR_API_KEY
+        lmt = 20
+        ckey = "my_test_app"  
+        search_term = "anime slap gif"
+        r = requests.get("https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (search_term, apikey, ckey,  lmt))
+        random_data = random.choice(r.json()["results"])
+        img = random_data["media_formats"]["gif"]["url"]
+        embed = Embed(title=f"{ctx.author.name} slaps {member.name} !", colour=ctx.author.color, timestamp=ctx.message.created_at)
+        embed.set_image(url=img)
+        if ctx.author.avatar != None:
+            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
+        else:
+            embed.set_author(name=ctx.author.name)
+        embed.set_footer(text=f"That's {slaps} slaps !")
+        await ctx.send(embed=embed)
+    
+    @commands.command(description="pat a member")
+    async def pat(self, ctx, *, member: discord.Member):
+        async with await psycopg.AsyncConnection.connect(host=host, dbname=database, user=user, password=password, port=port) as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("SELECT count(*) FROM INTERACTIONS where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                db_result = await cursor.fetchone()
+                if db_result[0] == 0:
+                    await cursor.execute(f"INSERT INTO INTERACTIONS VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (ctx.author.id, member.id, 0, 0, 0, 0, 0, 0,))
+                await db.commit()
+                await cursor.execute("SELECT NUM_PATS FROM INTERACTIONS where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                results = await cursor.fetchone()
+                pats = results[0] + 1
+                await cursor.execute("UPDATE INTERACTIONS set NUM_PATS = NUM_PATS + 1 where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                await db.commit()
+        apikey = TENOR_API_KEY
+        lmt = 20
+        ckey = "my_test_app"  
+        search_term = "anime pat gif"
+        r = requests.get("https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (search_term, apikey, ckey,  lmt))
+        random_data = random.choice(r.json()["results"])
+        img = random_data["media_formats"]["gif"]["url"]
+        embed = Embed(title=f"{ctx.author.name} pats {member.name} !", colour=ctx.author.color, timestamp=ctx.message.created_at)
+        embed.set_image(url=img)
+        if ctx.author.avatar != None:
+            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
+        else:
+            embed.set_author(name=ctx.author.name)
+        embed.set_footer(text=f"That's {pats} pats !")
+        await ctx.send(embed=embed)
+        
+    @commands.command(description="licks a member")
+    async def lick(self, ctx, *, member: discord.Member):
+        async with await psycopg.AsyncConnection.connect(host=host, dbname=database, user=user, password=password, port=port) as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("SELECT count(*) FROM INTERACTIONS where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                db_result = await cursor.fetchone()
+                if db_result[0] == 0:
+                    await cursor.execute(f"INSERT INTO INTERACTIONS VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (ctx.author.id, member.id, 0, 0, 0, 0, 0, 0,))
+                await db.commit()
+                await cursor.execute("SELECT NUM_LICKS FROM INTERACTIONS where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                results = await cursor.fetchone()
+                licks = results[0] + 1
+                await cursor.execute("UPDATE INTERACTIONS set NUM_LICKS = NUM_LICKS + 1 where USER1_ID = %s and USER2_ID = %s", (ctx.author.id, member.id))
+                await db.commit()
+        apikey = TENOR_API_KEY
+        lmt = 20
+        ckey = "my_test_app"  
+        search_term = "anime lick someone gif"
+        r = requests.get("https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (search_term, apikey, ckey,  lmt))
+        random_data = random.choice(r.json()["results"])
+        img = random_data["media_formats"]["gif"]["url"]
+        embed = Embed(title=f"{ctx.author.name} licks {member.name} !", colour=ctx.author.color, timestamp=ctx.message.created_at)
+        embed.set_image(url=img)
+        if ctx.author.avatar != None:
+            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
+        else:
+            embed.set_author(name=ctx.author.name)
+        embed.set_footer(text=f"That's {licks} licks !")
+        await ctx.send(embed=embed)
+    
+    ########################################## ERROR SECTION ###############################
+    @kiss.error
+    async def kiss_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = Embed(title="ERROR!", colour=ctx.author.color, timestamp=ctx.message.created_at)
+            embed.add_field(name="KISS ERROR", value="Please mention a specific member to kiss them !")
+            await ctx.send(embed=embed)
+            
+
+    @hug.error
+    async def hug_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = Embed(title="ERROR!", colour=ctx.author.color, timestamp=ctx.message.created_at)
+            embed.add_field(name="HUG ERROR", value="Please mention a specific member to hug them !")
+            await ctx.send(embed=embed)
+
+    @cuddle.error
+    async def cuddle_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = Embed(title="ERROR!", colour=ctx.author.color, timestamp=ctx.message.created_at)
+            embed.add_field(name="CUDDLE ERROR!", value="Please mention a specific member to cuddle them !")
+            await ctx.send(embed=embed)
+
+    @slap.error
+    async def slep_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = Embed(title="ERROR!", colour=ctx.author.color, timestamp=ctx.message.created_at)
+            embed.add_field(name="SLAP ERROR!", value="Please mention a specific member to slep them !")
+            await ctx.send(embed=embed)
+            
+    @pat.error
+    async def pat_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = Embed(title="ERROR!", colour=ctx.author.color, timestamp=ctx.message.created_at)
+            embed.add_field(name="PAT ERROR!", value="Please mention a specific member to pat them !")
+            await ctx.send(embed=embed)
+    
+    @lick.error
+    async def lick_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = Embed(title="ERROR!", colour=ctx.author.color, timestamp=ctx.message.created_at)
+            embed.add_field(name="LICK ERROR!", value="Please mention a specific member to lick them !")
+            await ctx.send(embed=embed)
+            
+    #################################################
+def setup(bot):
+    bot.add_cog(Interactions(bot)) 
---
+++ cogs/moderation.py
@@ -0,0 +1,75 @@
+import discord
+from discord.ext import commands
+from urllib.parse import urlparse
+import psycopg
+import asyncio
+import time
+import os
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+DB_URI = os.getenv("DB_URI")
+db_uri = urlparse(DB_URI)
+host = db_uri.hostname
+database = db_uri.path[1:]
+user = db_uri.username
+password = db_uri.password
+port= db_uri.port
+
+
+class Moderation(commands.Cog): 
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(description="Changes server prefix")
+    @commands.has_permissions(administrator=True)
+    async def setprefix(self, ctx: commands.Context, *, new_prefix: str):
+        async with await psycopg.AsyncConnection.connect(host=host, dbname=database, user=user, password=password, port=port) as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("UPDATE GUILD set GUILD_PREFIX = %s WHERE GUILD_ID = %s", (new_prefix, ctx.guild.id))
+                await db.commit()
+                await ctx.send("Prefix updated to `{}`".format(new_prefix))
+                
+    @commands.command(description="Clears the chat.")
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, num:int = 10):
+        def is_me(m):
+            m = True
+            return m
+        deleted = await ctx.channel.purge(limit=num, check=is_me)
+        message = await ctx.send(f'Deleted {len(deleted)} message(s) `This message will disappear after 3 seconds.`')
+        time.sleep(3)
+        await message.delete()
+        
+    @commands.command(description="kicks a user.")
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, *, member: discord.Member):
+        await member.kick()
+        await ctx.reply("**{0.name} has been kicked successfully**".format(member))
+        
+    @commands.command(description="bans a user.")
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member,*,reason: str = "Banned."):
+        await member.ban(delete_message_days=7, reason="Fuck off! ~banned by {0}~ for reason: {1}".format(ctx.author, reason))
+        await ctx.reply("**{0.name} has been banned successfully**".format(member))
+        
+        
+#------------------------- ERRORS --------------------#
+    @setprefix.error
+    async def setprefix_error(self, ctx, error):
+        await ctx.send("You don't have the permission to implement this command.")
+        
+    @clear.error
+    async def clear_error(self, ctx, error):
+        await ctx.send("You don't have the permission to implement this command.")
+        
+    @kick.error
+    async def kick_error(self, ctx, error):
+        await ctx.send("You don't have the permission to implement this command.")
+    
+    @ban.error
+    async def ban_error(self, ctx, error):
+        await ctx.send("You don't have the permission to implement this command.")
+        
+        
+def setup(bot):
+    bot.add_cog(Moderation(bot)) 
